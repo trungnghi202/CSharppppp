f63f589c44d089aee040b291cfc9384736a01f11
phantrungnghi <trungnghi202@gmail.com>
2022-12-20 15:04:07 +0700

    oke

--- OopEncapsulation/OopPolymorphismOverloadding/BasicMath.cs
+++ OopEncapsulation/OopPolymorphismOverloadding/BasicMath.cs
@@ -1,18 +1,36 @@
-﻿using System;
+﻿global using System;
 namespace OopPolymorphismOverloadding
 {
 	public class BasicMath
 	{
 			public int NumberOne { get; set; }
 			public int NumberTwo { get; set; }
-			public BasicMath()
-		{
+		//	public BasicMath()
+		//{
 
-		}
-		public BasicMath(int NumberOne,int NumberTwo)
+		//}
+		//public BasicMath(int numberOne,int numberTwo)
+		//{
+		//	numberOne = NumberOne;
+  //          numberTwo = NumberTwo;
+  //      }
+		//public BasicMath(int numberOne)
+		//{
+		//	numberOne = NumberOne;
+		//}
+  //      public BasicMath(int numberTwo)
+  //      {
+  //          numberTwo = NumberTwo;
+  //      }
+	//overloading phuong thuc
+	//optinal argument = >doi so truyen cho tham so(parameter)
+		public void sum(int num1 =1, int num2 = default)
 		{
-			numberOne = NumberOne;
+			NumberOne = num1;
+			NumberTwo = num2;
+			Console.WriteLine(NumberOne + NumberTwo);
 		}
+		
     }
 }
 
--- OopEncapsulation/OopPolymorphismOverloadding/Program.cs
+++ OopEncapsulation/OopPolymorphismOverloadding/Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OopPolymorphismOverloadding;
+
+BasicMath basic1 = new();
+BasicMath basic2 = new()
+{
+    NumberOne = 1 
+};
+BasicMath basic3 = new()
+{
+    NumberTwo = 11
+};
+BasicMath basic4 = new()
+{
+    NumberOne = 12,
+    NumberTwo = 21
+};
+
 
